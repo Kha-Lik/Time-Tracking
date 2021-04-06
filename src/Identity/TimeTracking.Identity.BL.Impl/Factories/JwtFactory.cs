e9ea99f0bd4f2da34dc652feb7076ab9ec651cda
Ira Tyschenko <tyschenk40@gmail.com>
2021-04-07 00:38:10 +0300

    unit Tests

--- src/Identity/TimeTracking.Identity.BL.Impl/Factories/JwtFactory.cs
+++ src/Identity/TimeTracking.Identity.BL.Impl/Factories/JwtFactory.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Security.Principal;
@@ -20,6 +21,7 @@ using TimeTracking.Identity.Entities;
 using TimeTracking.Identity.Models;
 using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
+[assembly: InternalsVisibleTo("Identity.UnitTests")]
 namespace TimeTracking.Identity.BL.Impl.Factories
 {
     internal sealed class JwtFactory : IJwtFactory
--- src/Identity/TimeTracking.Identity.Dal.Impl/TimeTrackingDbContext.cs
+++ src/Identity/TimeTracking.Identity.Dal.Impl/TimeTrackingDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TimeTracking.Common.Abstract.Repository;
 using TimeTracking.Identity.Entities;
 
 namespace TimeTracking.Identity.Dal.Impl
 {
-    public class TimeTrackingIdentityDbContext : IdentityDbContext<User, Role, Guid>
+    public class TimeTrackingIdentityDbContext : IdentityDbContext<User, Role, Guid>, IDbContext
     {
         public TimeTrackingIdentityDbContext(DbContextOptions<TimeTrackingIdentityDbContext> options) : base(options)
         {
