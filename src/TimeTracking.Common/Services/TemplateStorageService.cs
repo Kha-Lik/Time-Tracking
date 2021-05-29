97cac45b67c2e1350368600f71b9ef1e42415825
Ira Tyschenko <tyschenk40@gmail.com>
2021-05-29 16:51:02 +0300

    added logs

--- src/TimeTracking.Common/Services/TemplateStorageService.cs
+++ src/TimeTracking.Common/Services/TemplateStorageService.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Concurrent;
 using System.IO.Abstractions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using TimeTracking.Common.Abstract;
 using TimeTracking.Common.Enums;
  
@@ -11,13 +13,15 @@ namespace TimeTracking.Common.Services
     {
         private readonly IFileSystem _fileSystem;
         private readonly IWebHostEnvironment _webHostEnvironment;
- 
+        private readonly ILogger _logger;
+
         public TemplateStorageService(IFileSystem fileSystem, IWebHostEnvironment webHostEnvironment)
         {
             _fileSystem = fileSystem;
             _webHostEnvironment = webHostEnvironment;
+            ILoggerFactory loggerFactory = LoggerFactory.Create(c => c.AddDebug().AddConsole());
+            _logger = loggerFactory.CreateLogger<TemplateStorageService>();
         }
- 
         private ConcurrentDictionary<string, string> templateTypePathMap;
  
         private ConcurrentDictionary<string, string> TemplateTypePathMap
@@ -48,12 +52,20 @@ namespace TimeTracking.Common.Services
             string dir = "";
             try
             {
-                dir = _fileSystem.Path.Combine(_fileSystem.Directory.GetParent(_fileSystem.Directory.GetCurrentDirectory()).Parent.ToString(), "TimeTracking.Templates");
+                _logger.LogInformation($"Current dir: {_fileSystem.Directory.GetCurrentDirectory()}"+
+                                       $"Current dir parent: {_fileSystem.Directory.GetParent(_fileSystem.Directory.GetCurrentDirectory())}"+
+                                       $"Full dir path: {dir}");
+                dir = _fileSystem.Path.Combine(_fileSystem.Directory.GetParent(_fileSystem.Directory.GetCurrentDirectory()).
+                    Parent.ToString(), "TimeTracking.Templates");
             }
             catch (NullReferenceException nre)
             {
-                dir = _fileSystem.Path.Combine(_fileSystem.Directory.GetParent(_fileSystem.Directory.GetCurrentDirectory()).Name, "src/src/TimeTracking.Templates");
+                _logger.LogError(nre,$"Trying to fix directory issue according to docker context");
+                dir = _fileSystem.Path.Combine(_fileSystem.Directory.GetParent(_fileSystem.Directory.GetCurrentDirectory())
+                    .Name, "src/src/TimeTracking.Templates");
+                _logger.LogInformation($"Full dir path: {dir}");
             }
+            
             return _fileSystem.Path.Combine(dir, path.Replace('/', _fileSystem.Path.DirectorySeparatorChar));
         }
         public bool TryUpSertTemplate(string templateType, string path)
@@ -66,6 +78,7 @@ namespace TimeTracking.Common.Services
         {
             if (!TemplateTypePathMap.ContainsKey(templateKey))
             {
+                _logger.LogWarning($"{templateKey} not exists in TemplateTypePathMap");
                 return String.Empty;
             }
             return TemplateTypePathMap[templateKey];
@@ -75,10 +88,11 @@ namespace TimeTracking.Common.Services
         {
             if (!_fileSystem.File.Exists(path))
             {
+                _logger.LogWarning($"File with path {path} not exists");
                 return false;
             }
+            _logger.LogInformation($"File with path {path} is valid");
             return true;
         }
- 
     }
 }
